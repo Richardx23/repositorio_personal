491e10157ab14c53cc51f0f410e266d36f9a3ea0
Ricardo <00025325@uca.edu.sv>
2025-05-01 19:21:24 -0600

    ejemplo 2 de switch

---
+++ ejercicio2_switch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int opcion;
+    cout << "ingrese 1 para convertir de USD a EUR, 2 para USD a JPY y 3 para USD a GBP  ";
+    cin >> opcion;
+
+    double conversion, cantidad;
+    cout << "ingrese el valor monetario  ";
+    cin >> cantidad;
+    
+    
+
+    switch (opcion)
+    {case 1:
+        conversion = cantidad * 0.92;
+        cout << "su cantidad covertida es: " << conversion << " EUR" << endl;
+        break;
+    
+        case 2:
+        conversion = cantidad * 155;
+        cout << "su cantidad covertida es: " << conversion << " JPY" << endl;
+        break;
+
+        case 3:
+        conversion = cantidad * 0.70;
+        cout << "su cantidad es " << conversion << " GBP" << endl;
+        break;
+
+        default:
+        cout << "imposible" ;
+        break;
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+  return 0;
+}
