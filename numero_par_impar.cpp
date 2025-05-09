b14fa9d8fe88f34263788b5019f3d09a306bad9e
Richardx23 <00025325@uca.edu.sv>
2025-05-08 22:53:45 -0600

    comparacion de numeros y numeros pares e impares

---
+++ numero_par_impar.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "please enter a number to determine if it is an even or odd number  " << endl;
+    cin >> x;
+
+    if(x % 2 == 0)
+    cout <<  " your number is even" << endl;
+    
+    else 
+    {
+    cout << "your numer is odd" << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
